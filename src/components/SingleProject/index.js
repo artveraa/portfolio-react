bb14d91e01a709aed5ff559ab39bc56d19ddc474
Arthur Vera <arthur@mcube.fr>
2023-04-27 16:43:34 +0200

    Single

--- src/components/SingleProject/index.js
+++ src/components/SingleProject/index.js
@@ -1,11 +1,24 @@
-import { useParams } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import createClient from "../../client";
 
 const SingleProject = () => {
+    const [singleProject, setSingleProject] = useState([]);
+
+    useEffect(() => {
+        createClient.fetch(`*[slug.current == "${slug}"]{
+            title,
+            slug,
+        } | order(publishedAt desc)`).then((data) => setSingleProject(data[0]));
+    });
+
     const {slug} = useParams();
+
     return (
         <>
             <h1>Single Project</h1>
-            <p>{slug}</p>
+            {/*<p>{slug}</p>*/}
+            <h2>{singleProject.title}</h2>
         </>
     );
 }
